2b0910fb4911bf5a95bd5195cd1679e0ed588717
nicolaydammer <nicolaydammer@gmail.com>
2022-04-15 14:37:34 +0200

    CHANGE: Changed template structure and added login and register route

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,20 @@ from app import app
 @app.route('/')
 def index():
     return render_template
+
+
+@app.route('/login')
+def login():
+    return render_template(
+        'base.html',
+        title="Filmfan login pagina",
+        page="login"
+    )
+
+@app.route('/register')
+def register():
+    return render_template(
+        'base.html',
+        title="Filmfan register pagina",
+        page="register"
+    )
