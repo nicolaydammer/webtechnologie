a1b72278a0b610ddd72aa7d2e2b66d798961f23c
SjoerdFroma <sj.froma@st.hanze.nl>
2022-04-15 18:47:04 +0200

    probleem

--- app/routes.py
+++ app/routes.py
@@ -4,7 +4,33 @@ from app import app
 
 @app.route('/')
 def index():
-    return render_template
+    return render_template(
+        'base.html',
+        title="Filmfan homepage",
+        page="home",
+        cards = [
+	        {
+		     'id':'12',
+		      'date':'15-4-2022'
+	        },
+	        {
+		     'id':'15',
+		      'date':'16-4-2022'
+	        },
+	        {
+		     'id':'18',
+		      'date':'17-4-2022'
+	        },
+            {
+		     'id':'21',
+		      'date':'18-4-2022'
+	        },
+            {
+		     'id':'24',
+		      'date':'19-4-2022'
+	        },
+        ]
+    )
 
 
 @app.route('/login')
