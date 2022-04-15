b55e4c6577cf7252d843a8554be826170afe1578
nicolaydammer <nicolaydammer@gmail.com>
2022-04-15 19:17:04 +0200

    ADD: Added authentication

--- app/Models/Regisseur.py
+++ app/Models/Regisseur.py
@@ -2,6 +2,8 @@ from app import db
 
 
 class Regisseur(db.Model):
+    __tablename__ = 'regisseurs'
+
     id = db.Column(db.Integer, primary_key=True)
     voornaam = db.Column(db.String(50))
     achternaam = db.Column(db.String(50))
--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,35 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 
 @app.route('/')
 def index():
-    return render_template
-
-
-@app.route('/login')
-def login():
     return render_template(
         'base.html',
         title="Filmfan login pagina",
-        page="login"
+        page="home",
     )
 
-@app.route('/register')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template(
+            'base.html',
+            title="Filmfan login pagina",
+            page="login"
+        )
+    if request.method == 'POST':
+        return 'hi'
+
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template(
-        'base.html',
-        title="Filmfan register pagina",
-        page="register"
-    )
+    if request.method == 'GET':
+        return render_template(
+            'base.html',
+            title="Filmfan register pagina",
+            page="register"
+        )
+    if request.method == 'POST':
+        return 'hi'
