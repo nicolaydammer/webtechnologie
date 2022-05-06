dbeb520126e71c08f86f685987c7cf75a018bbbb
nicolaydammer <nicolaydammer@gmail.com>
2022-05-06 14:30:16 +0200

    ADD: database update for film beschrijving

--- app/Models/Film.py
+++ app/Models/Film.py
@@ -6,11 +6,13 @@ class Film(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     titel = db.Column(db.String(50))
+    beschrijving = db.Column(db.String(100))
     regisseur = db.Column(db.Integer, db.ForeignKey('regisseurs.id'))
     acteur = db.Column(db.Integer, db.ForeignKey('acteurs.id'))
     comment = db.Column(db.Integer, db.ForeignKey('comment.id'))
 
-    def __init__(self, titel, regisseur, acteur):
+    def __init__(self, titel, regisseur, acteur, beschrijving):
         self.titel = titel
         self.regisseur = regisseur
         self.acteur = acteur
+        self.beschrijving = beschrijving
